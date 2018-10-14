7bd0380a6a56d04e9465002e9baaff60f2270f72
Frikikiki <33179178+Frikikiki@users.noreply.github.com>
2018-10-14 14:14:18 +0300

    Добавление контекста и первой сущности БД (#12)
    * Added context and first entity for DB
    
    * Исправил ошибки
    
    * Исправление ошибок
    
    * Revert "Исправление ошибок"
    
    This reverts commit 167ef541fa5fc64f0e2c17fbec7a9240180652a7.
    
    * Добавление internal

---
+++ Food.EntityFramework/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Food.EntityFramework.Entities
+{
+    internal class User : IEntity
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
---
+++ Food.EntityFramework/FoodDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Food.EntityFramework.Context
+{
+    class internal FoodDbContext : DbContext
+    {
+        public DbSet<Entities.User> Users { get; set; }
+    }
+}
