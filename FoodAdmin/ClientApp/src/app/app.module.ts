c8b5c7839b283e94036d7c8fd796a010fbc18684
AnastasiaBastrakova <nastyabastrakova@gmail.com>
2018-10-15 18:06:12 +0300

    Revert "Изменила UI админки (#22)"
    This reverts commit b21bf89ce37c0f38362bbd7bb00f3062cd90795b.

--- FoodAdmin/ClientApp/src/app/app.module.ts
+++ FoodAdmin/ClientApp/src/app/app.module.ts
@@ -5,21 +5,27 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from '../nav-menu/nav-menu.component';
-import { FoodListComponent } from '../food-list/food-list.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { CounterComponent } from './counter/counter.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    FoodListComponent,
+    HomeComponent,
+    CounterComponent,
+    FetchDataComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: FoodListComponent, pathMatch: 'full' },
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'counter', component: CounterComponent },
+      { path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
   providers: [],
