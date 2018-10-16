36a0cc8986693179607533f7a4764d37a1f8d110
SherlockD <ryavi1@mail.com>
2018-10-16 11:05:02 +0300

    Add IQueryable type

--- Food.EntityFramework/IRepository.cs
+++ Food.EntityFramework/IRepository.cs
@@ -5,7 +5,7 @@ namespace Food.EntityFramework
 {
     internal interface IRepository<TEntity> where TEntity : IEntity
     {
-        IQueryable All { get; }
+        IQueryable<TEntity> All { get; }
         TEntity GetItem(int id);
         TEntity Save(TEntity item);
         void Delete(TEntity item);
