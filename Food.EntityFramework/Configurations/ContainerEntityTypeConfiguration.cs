d4eea6ec1c6d5c628f1852ac36f5cc56b1bf6ada
AnastasiaBastrakova <nastyabastrakova@gmail.com>
2018-10-19 17:06:07 +0300

    Подправила конфигурацию сущностей

---
+++ Food.EntityFramework/Configurations/ContainerEntityTypeConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Food.EntityFramework.Entities.Configurations
+{
+    class ContainerConfiguration : IEntityTypeConfiguration<Container>
+    {
+        public void Configure( EntityTypeBuilder<Container> builder )
+        {
+            builder.ToTable("Containers").HasKey(container => container.Id);
+            builder.Property(container => container.Name).IsRequired().HasMaxLength(40);
+            builder.Property(container => container.Price).IsRequired();
+        }
+    }
+}
---
+++ Food.EntityFramework/Configurations/OrderEntityTypeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Food.EntityFramework.Entities.Configurations
+{
+    class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure( EntityTypeBuilder<Order> builder )
+        {
+            builder.ToTable("Orders").HasKey(order => order.Id);
+            builder.Property(order => order.TotalSum);
+            builder.Property(order => order.OrderDate.Date);
+            builder.Property(order => order.MenuId);
+            builder.Property(order => order.DeliveryDate);
+            builder.Property(order => order.UserId);
+        }
+    }
+}
