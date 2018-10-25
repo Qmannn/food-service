a7174a535ebbeb5dd48422bf6d902f891056da67
Grigor <grigor513@outlook.com>
2018-10-25 22:28:49 +0300

    Added migration files

--- Food.EntityFramework/FoodDbContext.cs
+++ Food.EntityFramework/FoodDbContext.cs
@@ -2,15 +2,19 @@
 using System;
 using System.Reflection;
 using System.Linq;
-
+using JetBrains.Annotations;
 
 namespace Food.EntityFramework.Context
 {
-    internal sealed class FoodDbContext : DbContext
+    public class FoodDbContext : DbContext
     {
         public FoodDbContext()
         {
             Database.EnsureCreated();
+        }
+
+        public FoodDbContext(DbContextOptions<FoodDbContext> options) : base(options)
+        {
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
