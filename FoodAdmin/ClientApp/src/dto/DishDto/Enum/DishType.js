32778a5d93d5681c5f682c6a74a78ffa0a4bb65d
Vladimir <kozyrev-vova@mail.ru>
2018-11-22 22:49:30 +0300

    Добавление в dto отдельного файла с enum

---
+++ FoodAdmin/ClientApp/src/dto/DishDto/Enum/DishType.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var DishType;
+(function (DishType) {
+    DishType[DishType["Salad"] = 0] = "Salad";
+    DishType[DishType["Soup"] = 1] = "Soup";
+    DishType[DishType["SecondDish"] = 2] = "SecondDish";
+    DishType[DishType["Garnish"] = 3] = "Garnish";
+})(DishType = exports.DishType || (exports.DishType = {}));
+//# sourceMappingURL=DishType.js.map
