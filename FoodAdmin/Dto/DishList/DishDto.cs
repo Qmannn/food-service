d80c58ab144507abae2cfee3a925f4c57f055cba
Vladimir <kozyrev-vova@mail.ru>
2018-11-24 00:31:54 +0300

    Поменял DishList на DishDto

--- FoodAdmin/Dto/DishList/DishDto.cs
+++ FoodAdmin/Dto/DishList/DishDto.cs
@@ -1,11 +1,11 @@
 ﻿using FoodAdmin.Dto.DishList.Enum;
 
-namespace FoodAdmin.Dto.DishList
+namespace FoodAdmin.Dto.DishDto
 {
     public class DishDto
     {
         public string DishName { get; set; }
-        public int DishPrice { get; set; }
+        public decimal DishPrice { get; set; }
         public int DishWeight { get; set; }
         public DishType DishType { get; set; }
     }
