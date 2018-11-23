5ae2b29cf90b57c6d2d9c9d7f61f4dec02a300c0
Vladimir <kozyrev-vova@mail.ru>
2018-11-24 00:33:38 +0300

    Поменял int на decimal и поставил данные значения для теста

--- FoodAdmin/Controllers/DishListController/DishListController.cs
+++ FoodAdmin/Controllers/DishListController/DishListController.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using FoodAdmin.Dto.DishList;
+using FoodAdmin.Dto.DishDto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FoodAdmin.Controllers
@@ -16,35 +16,35 @@ namespace FoodAdmin.Controllers
                 new DishDto
                 {
                     DishName = "Блинчики с клубничной начинкой",
-                    DishPrice = 55,
+                    DishPrice = 55.0m,
                     DishType = Dto.DishList.Enum.DishType.SecondDish,
                     DishWeight = 140
                 },
                 new DishDto
                 {
                     DishName = "Буженина из баранины с соусом",
-                    DishPrice = 180,
+                    DishPrice = 180.0m,
                     DishType = Dto.DishList.Enum.DishType.Garnish,
                     DishWeight = 100
                 },
                 new DishDto
                 {
                     DishName = "Макароны отварные с овощами",
-                    DishPrice = 16,
+                    DishPrice = 16.0m,
                     DishType = Dto.DishList.Enum.DishType.Garnish ,
                     DishWeight = 200
                 },
                 new DishDto
                 {
                     DishName = "Рассольник Ленинградский",
-                    DishPrice = 52,
+                    DishPrice = 52.89m,
                     DishType = Dto.DishList.Enum.DishType.Soup,
                     DishWeight = 250
                 },
                 new DishDto
                 {
                     DishName = "Салат Кировский",
-                    DishPrice = 68,
+                    DishPrice = 68.5m,
                     DishType = Dto.DishList.Enum.DishType.Salad,
                     DishWeight = 120
                 },
