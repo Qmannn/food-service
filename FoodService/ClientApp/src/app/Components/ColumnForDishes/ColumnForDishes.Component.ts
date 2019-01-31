e503f3796fa60be8dcc5dbd76febb67fda73e12e
Vladimir <kozyrev-vova@mail.ru>
2019-01-31 17:35:52 +0300

    тестовый компонент

---
+++ FoodService/ClientApp/src/app/Components/ColumnForDishes/ColumnForDishes.Component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-column-dishes',
+  templateUrl: './ColumnForDishes.Component.html',
+  styleUrls: ['./ColumnForDishes.Component.css']
+})
+export class ColumnForDishesComponent {
+
+}
--- FoodService/ClientApp/src/app/app.module.ts
+++ FoodService/ClientApp/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { ColumnForDishesComponent } from './Components/ColumnForDishes/ColumnForDishes.Component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    ColumnForDishesComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
