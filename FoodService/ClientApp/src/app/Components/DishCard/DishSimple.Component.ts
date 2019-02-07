cdcda6447ad7393bed8c1305fe82b65a501c4c15
ondrosru <izickovondros@gmail.com>
2019-02-07 13:59:26 +0300

    ISSUE-56 Добавил блок для карточки.

--- FoodService/ClientApp/src/app/Components/DishCard/DishSimple.Component.ts
+++ FoodService/ClientApp/src/app/Components/DishCard/DishSimple.Component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { DishDto } from '../../../dto/Dish/DishDto';
-import { DishCardComponent } from './DishCard.Component';
 
 
 @Component({
     templateUrl: './DishSimple.Component.html',
+    styleUrls: ['./DishSimple.Component.css'],
 })
 
 export class DishSimpleComponent {
