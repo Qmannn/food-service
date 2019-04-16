9d3aafc369e84ed8f513e96f1dbdd0705cc1a35b
glcherepanov <32065951+glcherepanov@users.noreply.github.com>
2019-04-16 09:19:31 +0300

    ISSUE-88 Добавил preloader (#89)

--- FoodAdmin/ClientApp/src/HttpServices/HttpService.ts
+++ FoodAdmin/ClientApp/src/HttpServices/HttpService.ts
@@ -1,20 +1,40 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Inject, Injectable, NgZone } from '@angular/core';
+import { Inject, Injectable, NgZone} from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { tap, catchError } from 'rxjs/operators';
 
 @Injectable()
 export class HttpService {
     private readonly _httpClient: HttpClient;
     private readonly _baseUrl: string;
+    private bodyElement = document.getElementsByClassName('body-content')[0];
+    private preloaderElement = document.createElement('div');
+    private errorElement = document.createElement('div');
 
     public constructor(httpService: HttpClient, @Inject('BASE_URL') baseUrl: string, private zone: NgZone) {
         this._httpClient = httpService;
         this._baseUrl = baseUrl;
     }
 
+    private addDiv(element: HTMLDivElement, className: string): void {
+        element.className = className;
+        this.bodyElement.appendChild(element);
+    }
+
+    private removeDiv(element: HTMLDivElement): void {
+        this.bodyElement.removeChild(element);
+    }
+
     public get<T>(url: string, params?: HttpParams): Observable<T> {
+        this.addDiv(this.preloaderElement, 'preloader');
         const httpHeaders = new HttpHeaders().set('Accept', 'application/json');
-        return this._httpClient.get<T>(this._baseUrl + url, {headers: httpHeaders, params: params});
+        return this._httpClient.get<T>(this._baseUrl + url, {headers: httpHeaders, params: params}).pipe(
+            tap(_ => {
+                this.removeDiv(this.preloaderElement);
+            }, () => {
+                this.removeDiv(this.preloaderElement);
+            })
+        );
     }
 
     public post<TRq, TRs>(url: string, body: TRq): Observable<TRs> {
